413b6d053c7469fcb8f8825c69bca18903e74f7c
rabi <wrabwagle123@gmail.com>
2024-08-09 19:03:16 +0545

    authguard

--- Node_js/middleware/authGaurd.js
+++ Node_js/middleware/authGaurd.js
@@ -1,40 +1,6 @@
 
 const jwt = require ('jsonwebtoken');
 
-const authGuard = (req,res,next)=> {
-    // get header  authorization
-
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.json({
-            sucess:false,
-            message :"Authorization header not found !"
-        })
-    }
-
-    // get token by spliting the header
-    // Format = "Bearer tokenxyfghjhgfdfghg"
-    const token = authHeader.split (' ')[1];
-    if(!token){
-        return res.json({
-            sucess:false,
-            message:"Token not found!"
-        })
-    }
-    try{
-        //verify token 
-        const decodeUser = jwt.verify(token,"HFBDFVRGSGVF");
-        req.user = decodeUser;
-        next();
-
-    }catch (error){
-        res.json({
-            sucess:false,
-            message:"Invalid Token"
-        })
-    }
-
-}
 
 
 const authGuardAdmin = (req,res,next)=> {
